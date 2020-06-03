0397de81e484431e22fa9b3d8295a59da18063d1
yimjisu <60869086+yimjisu@users.noreply.github.com>
2020-06-03 16:30:56 +0900

    Add files via upload

---
+++ DP4.js
@@ -0,0 +1,30 @@
+// This allows the Javascript code inside this block to only run when the page
+// has finished loading in the browser.
+
+$( document ).ready(function() {
+  });
+
+var restaurant = document.getElementById("restaurant");
+var season = document.getElementById("season");
+function bindEvents(){
+  restaurant.onclick = function(){
+    $(this).css("background-color", "#2BC78C");
+    $(season).css("background-color", "#8FECC9");
+    restaurant();
+  }
+  season.onclick = function(){
+    $(this).css("background-color", "#2BC78C");
+    $(restaurant).css("background-color", "#8FECC9");
+    season();
+  }
+}
+
+function restaurant(){
+
+}
+
+function season(){
+  
+}
+
+bindEvents();
