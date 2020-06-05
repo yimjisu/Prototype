08246a1d9d50168c0829b41c8eee5819f271589b
Neila-ros <55741753+Neila-ros@users.noreply.github.com>
2020-06-05 23:58:13 +0900

    Update season.js

--- season.js
+++ season.js
@@ -20,16 +20,47 @@ function bindEvents(){
   }
 }
 
-function select_ing(){
-  var selected_ings=[];
-  var ings = document.getElementsByTagName("input");
+var selected_ings=[];
+
+var select_btn = document.getElementById("select");
+
+select_btn.addEventListener('click', function(){
+  var ings = document.getElementsByTagName("input")
+  selected_ings=[];
   for(var i=0; i < ings.length; i++){
     if (ings[i].checked == true){
       selected_ings.push(ings[i].id);
     }
   }
-  //for our prototype, we only care if only spinach is selected
   if ((selected_ings.length == 1) && (selected_ings[0]=="Spinach")){
-    console.log("spinach checked", selected_ings);
+    window.location.replace("./spinach.html");
   }
-}
+});
+
+var show_v=0;
+var show_veg = document.getElementById("show_more");
+show_veg.addEventListener('click', function(){
+  if (!show_v){
+    document.getElementById("more").style.display = "block";
+    show_veg.innerHTML="Show less"
+    show_v=1;
+  }else{
+    document.getElementById("more").style.display = "none";
+    show_veg.innerHTML="Show more"
+    show_v=0;
+  }
+});
+
+var show_f=0;
+var show_fruits = document.getElementById("show_more2");
+show_fruits.addEventListener('click', function(){
+  if (!show_f){
+    document.getElementById("more2").style.display = "block";
+    show_fruits.innerHTML="Show less"
+    show_f=1;
+  }else{
+    document.getElementById("more2").style.display = "none";
+    show_fruits.innerHTML="Show more"
+    show_f=0;
+  }
+});
