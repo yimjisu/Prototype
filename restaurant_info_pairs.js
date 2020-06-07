0d1be9e84902c60474fc51897f7d25e3020fbb60
StephenCurry520 <953982715@qq.com>
2020-06-07 17:52:26 +0800

    https://github.com/yimjisu/Prototype/tree/restaurant

---
+++ restaurant_info_pairs.js
@@ -0,0 +1,20 @@
+var pairs = [
+    {
+        "name": "Veggie Paradise",
+        "category": "Vegetarian",
+        "location": "Seoul",
+        "price": 10000,
+    },
+    {
+        "name": "Plant Food Land",
+        "category": "Vegetarian",
+        "location": "Busan",
+        "price": 16000,
+    },
+    {
+        "name": "Moe's",
+        "category": "Vegetarian",
+        "location": "Daejeon",
+        "price": 14000,
+    },
+]
