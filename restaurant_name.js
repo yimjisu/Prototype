e51999217dfdcebf15fd37130d183e634448c05f
Neila-ros <55741753+Neila-ros@users.noreply.github.com>
2020-07-01 16:38:59 +0900

    Send Message -> Join Group

--- restaurant_name.js
+++ restaurant_name.js
@@ -49,7 +49,7 @@ function findgroup(){
   cell2 = numberRow.insertCell(1);
   cell2.align = 'right';
   sendMessage = document.createElement('button');
-  sendMessage.innerHTML = '<b>Send Message</b>';
+  sendMessage.innerHTML = '<b>Join Group</b>';
   sendMessage.setAttribute("id", restaurantName+" #"+j.toString());
   sendMessage.setAttribute("class", j.toString());
   sendMessage.onclick = function(){    
